c696f166f5de1733accd928483571e9a5cc0ebca
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-05-05 22:31:02 +0200

    Add some test to text-fixture.

--- tests/fixtures/logging1_in.py
+++ tests/fixtures/logging1_in.py
@@ -9,6 +9,10 @@ a = b = 1
 
 class X(object, log.Loggable):
     def x():
+        logging.debug('', a, b, c)
+
         logging.debug('%s %r %s', 1, a+b, c.e())
         logging.info('%s %r', 1, a+b, c.e())
         logging.error('%s', 1, a+b, c.e())
+
+        logging.error("%s", a)
--- tests/fixtures/logging1_out.py
+++ tests/fixtures/logging1_out.py
@@ -11,6 +11,10 @@ a = b = 1
 
 class X(object, log.Loggable):
     def x():
+        logging.debug(' %s %s %s', a, b, c)
+
         logging.debug('%s %r %s', 1, a + b, c.e())
         logging.info('%s %r %s', 1, a + b, c.e())
         logging.error('%s %s %s', 1, a + b, c.e())
+
+        logging.error("%s", a)
